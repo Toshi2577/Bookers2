daef2dae4c30df2dbca61b4fe418c5cc6284ea20
EC2 Default User <ec2-user@ip-172-31-18-123.ec2.internal>
2022-10-18 06:42:35 +0000

    third comit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  def index
+    @users = User.all
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+end
